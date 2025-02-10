23abf009a206a7177eead18083d0fa84cb6cd569
moonieym <monseb1219@gmail.com>
2025-02-09 20:33:38 -0600

    agrego cambios en el codigo

--- 06-02-25/script2.js
+++ 06-02-25/script2.js
@@ -10,23 +10,23 @@ console.log(potencia); // 8
 
 //suma
 let suma = 2 + 3
-console.log(suma)
+console.log(suma) // 5
 
 //resta 
 let resta = 2 - 3
-console.log(resta)
+console.log(resta) // -1
 
 //multiplicacion
 let multiplicacion = 2 * 3
-console.log(multiplicacion)
+console.log(multiplicacion) // 6
 
 //division
 let division = 2 / 3
-console.log(division)
+console.log(division) // 0.666
 
 //modulo
 let modulo = 2 % 3
-console.log(modulo)
+console.log(modulo) // 2
 
 //combinados
 
@@ -46,7 +46,7 @@ i = i + 1
 
 i += 1
 i++
-console.log(i)
+console.log(i) // 5
 
 //decremento
 let j = 100
@@ -57,6 +57,6 @@ j = j - 1
 
 j -= 1
 j--
-console.log(j)
+console.log(j) //95
 
 variable = "variable"
