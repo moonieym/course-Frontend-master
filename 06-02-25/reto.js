77c77de1dd4e90ce07ef44c647a4e2bfa4bae9d1
moonieym <monseb1219@gmail.com>
2025-02-19 19:30:43 -0600

    agrego cambios en el codigo js

---
+++ 06-02-25/reto.js
@@ -0,0 +1,15 @@
+let a = 5
+let b = 3
+
+if(a - b > 3) {
+    if(b - a == 2) {
+    console.log("RES B")
+    } else {
+        let c = b * a
+        if(c > 15) {
+            console.log("RES A")
+        }
+    }
+    console.log("RES C")
+}
+
