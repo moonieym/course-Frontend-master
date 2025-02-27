375804af1c0eb8ebf4c02031ca6e1e2d94cac7eb
moonieym <monseb1219@gmail.com>
2025-02-26 21:46:18 -0600

    agrego cambios en codigo js

---
+++ 18-02-25/localizar_persona.js
@@ -0,0 +1,7 @@
+// Buscador: ______________
+
+let lista_de_personas = [
+    {nombre: "Nestor", apellido: "Garcia", edad: 25},
+    {nombre: "Dana", apellido: "Martinez", edad: 28},
+    {nombre: "Jose", apellido: "Perez", edad: 30},
+    
