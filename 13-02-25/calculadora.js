953d5abdb10a36d02501f28fb72f519556319865
moonieym <monseb1219@gmail.com>
2025-02-26 19:35:43 -0600

    agrego cambios en codigo js

---
+++ 13-02-25/calculadora.js
@@ -0,0 +1,22 @@
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
+
+//calcular
+
+console.log("Suma: " + sumar(15, 10));
+console.log("Resta: " + restar(28, 9));
+console.log("Multiplicacion: " + multiplicar(5, 12));
+console.log("Division: " + dividir(49, 7));
