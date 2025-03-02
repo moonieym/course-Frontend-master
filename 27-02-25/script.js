468b06a32aecf235d1f3df6a8104828f42ae4f27
moonieym <monseb1219@gmail.com>
2025-03-01 23:58:19 -0600

    agrego cambios en codigo js

--- 27-02-25/script.js
+++ 27-02-25/script.js
@@ -2,7 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("commentForm");
     const commentInput = document.getElementById("commentInput");
     const commentList = document.getElementById("commentList");
+    const themeToggle = document.getElementById("themeToggle");
 
+    // Cambia el tema al hacer clic en el botón de cambio de tema
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+        themeToggle.textContent = "🌞";
+    }
+
+    // Cambia el tema al hacer clic en el botón de cambio de tema
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+            themeToggle.textContent = "🌞";
+        } else {
+            localStorage.setItem("theme", "light");
+            themeToggle.textContent = "🌙";
+        }
+    });
+
+    // agrega el comentario
     form.addEventListener("submit", (event) => {
         event.preventDefault(); // Evita el envío del formulario
 
