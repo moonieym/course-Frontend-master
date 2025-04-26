88512c8d01b532c5f6bb3b2a73b1855ee7c45e24
moonieym <monseb1219@gmail.com>
2025-04-25 21:23:58 -0600

    agrego codigo a repo

--- modulo_4/03-04-25/reto.js
+++ modulo_4/03-04-25/reto.js
@@ -1,5 +1,48 @@
 // Fibonacci
 
 function fibo(n) {
-    //
+    //Caso(s) base
+    if(n === 0) {
+        return 0
+    }
+
+    if(n === 1) {
+        return 1
+    }
+
+    /*
+    if(n === 2) {
+        return 1
+    }
+
+    if(n === 3) {
+        return 2
+    }
+
+    if(n === 4) {
+        return 3
+    }
+
+    if(n === 5) {
+        return 5
+    }
+
+    if(n === 6) {
+        return fibo(6-2) + fibo(6-1) 
+    }
+
+    if(n === 7) {
+        return fibo(7-2) + fibo(7-1) 
+    }
+    */
+
+    // Llamada recursiva
+    return fibo(n-2) + fibo(n-1) /* */
+
 }
+
+
+// Primera llamada
+// n representa el valor en la posicion de la secuencia
+let result = fibo(7) // 21
+console.log(result)
