ab18a2847afd0ca7dd84dc95f8c0ddc5a4bc1658
moonieym <monseb1219@gmail.com>
2025-05-10 15:32:48 -0600

    agrego codigo js a repo

--- modulo_5/08-05-25/album/script.js
+++ modulo_5/08-05-25/album/script.js
@@ -1,8 +1,10 @@
 const imagesContainer = document.getElementById("images-container")
+const uploadForm = document.getElementById("upload-form")
 const renderImage = (imageObject) => {
     return `
         <div>
             <h4>${imageObject.titulo}</h4>
+            <img src=${imageObject.foto} />
         </div>
     `
 }
@@ -11,15 +13,46 @@ const requestOptions = {
     method: "GET",
     redirect: "follow"
 };
-  
-fetch("https://sheetdb.io/api/v1/1zdjycnz0gdy2", requestOptions)
-    .then((response) => response.json())
-    .then((images) => {
-        let html = ""
-        for(let i = 0; i<images.length; i++) {
-            html += renderImage(images[i])
-        }
-        console.log(html)
-        imagesContainer.innerHTML = html
+
+const loadImages = () => {
+    fetch("https://sheetdb.io/api/v1/1zdjycnz0gdy2", requestOptions)
+        .then((response) =>response.json())
+        .then((images) => {
+            let html = ""
+            for(let i = 0; i<images.length; i++) {
+                html += renderImage(images[i])
+            }
+            console.log(html)
+            imagesContainer.innerHTML = html
+        })
+        .catch((error) => console.error(error));
+}
+
+uploadForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    console.log(e.target.title.value)
+    console.log(e.target.imageBase64.value)
+
+    const raw = JSON.stringify({
+        id: "=ROW()",
+        titulo: e.target.title.value,
+        foto: e.target.imageBase64.value
     })
-    .catch((error) => console.error(error));
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    fetch("https://sheetdb.io/api/v1/1zdjycnz0gdy2", {
+        headers: myHeaders,
+        method: "POST",
+        body: raw
+    }).then((response) => response.json())
+    .then((result) => {
+        console.log(result)
+        loadImages()
+    })
+    .catch((error) => console.error(error))
+})
+
+loadImages()
