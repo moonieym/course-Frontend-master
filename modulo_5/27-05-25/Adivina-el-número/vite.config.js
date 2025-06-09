807dc702f8fab686bfaa222557a481e49a7dcf46
moonieym <monseb1219@gmail.com>
2025-06-08 20:41:53 -0600

    agrego cambios a repo

--- modulo_5/27-05-25/Adivina-el-número/vite.config.js
+++ modulo_5/27-05-25/Adivina-el-número/vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'course-Frontend-master/modulo_5/27-05-25/Adivina-el-número/', // <- Usa el nombre EXACTO de tu repo
+  base: '/course-Frontend-master/modulo_5/27-05-25/Adivina-el-número/', // <- Usa el nombre EXACTO de tu repo
 })
