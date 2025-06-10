d51cf3e8eb47c699f98c76c0c6215e512ee2a041
moonieym <monseb1219@gmail.com>
2025-06-10 00:32:46 -0600

    agrego cambios en repo

--- modulo_6/03-06-25/mi-tarjeta/src/Tarjeta.jsx
+++ modulo_6/03-06-25/mi-tarjeta/src/Tarjeta.jsx
@@ -1,7 +1,8 @@
+import miFoto from './assets/mi-foto.jpg';
 function Tarjeta({ nombre, profesion, mensaje }) {
   return (
     <div style={{ border: '1px solid #ccc', padding: '20px', width: '300px', textAlign: 'center' }}>
-      <img src="https://via.placeholder.com/100" alt="Foto de perfil" style={{ borderRadius: '50%' }} />
+      <img src={miFoto} alt="Foto de perfil" style={{ borderRadius: '50%', width: '100px', height: '100px' }} />
       <h2>{nombre}</h2>
       <h4>{profesion}</h4>
       <p>{mensaje}</p>
