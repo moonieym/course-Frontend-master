8820b8f55fcd05da9f4d39a9de9e68671dcfc0c2
moonieym <monseb1219@gmail.com>
2025-08-07 18:39:35 -0600

    agrego cambios en codigo

--- modulo_6/26-06-25/mi-twitter-clon/src/components/TweetForm.jsx
+++ modulo_6/26-06-25/mi-twitter-clon/src/components/TweetForm.jsx
@@ -1,43 +1,28 @@
 import { useState } from "react";
 
 const TweetForm = ({ onAddTweet }) => {
-
   const [text, setText] = useState("");
 
   const handleSubmit = (e) => {
-
     e.preventDefault();
-
     if (!text.trim()) return;
-
     onAddTweet(text);
-
     setText("");
-
   };
 
   return (
-
-    <form onSubmit={handleSubmit}>
-
-      <input
-
-        type="text"
-
-        value={text}
-
-        onChange={(e) => setText(e.target.value)}
-
-        placeholder="¿Qué estás pensando?"
-
-      />
-
-      <button type="submit">Tweet</button>
-
-    </form>
-
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="¿Qué estás pensando?"
+        />
+        <button type="submit">Tweet</button>
+      </form>
+    </div>
   );
-
 };
 
 export default TweetForm;
